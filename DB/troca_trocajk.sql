c143205273e82ca5023fcb208206fd9d87b2b99a
isackalmeida <isacks.almeida@gmail.com>
2025-05-02 16:16:37 -0300

    Update troca_trocajk.sql

--- DB/troca_trocajk.sql
+++ DB/troca_trocajk.sql
@@ -1,84 +1,31 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 28/03/2025 às 04:06
--- Versão do servidor: 8.0.41
--- Versão do PHP: 8.2.12
+CREATE DATABASE troca_trocaJK;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+USE troca_trocaJK;
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `troca_trocajk`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `usuarios`
---
-
-CREATE TABLE `usuarios` (
-  `id` int NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `nome_sobrenome` varchar(255) NOT NULL,
-  `nome_usuario` varchar(50) NOT NULL,
-  `senha_hash` varchar(255) NOT NULL,
-  `data_cadastro` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices de tabela `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- AUTO_INCREMENT para tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- Estrutura para tabela `livros`
---
-
-CREATE TABLE `livros` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `titulo` VARCHAR(255),
-  `autor` VARCHAR(255),
-  `genero` VARCHAR(100),
-  `estado` VARCHAR(50),
-  `imagem` BLOB,
-  `usuario_id` INT,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`usuario_id`) REFERENCES `usuario`(`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    nome_sobrenome varchar(255) not null,
+    nome_usuario VARCHAR(50) NOT NULL,
+    senha_hash VARCHAR(255) NOT NULL,
+    foto_perfil VARCHAR(255) DEFAULT NULL,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
 
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
+CREATE TABLE livros (
+   id INT NOT NULL AUTO_INCREMENT,
+  titulo VARCHAR(255),
+  autor VARCHAR(255),
+  genero VARCHAR(100),
+  estado VARCHAR(50),
+  imagens varchar (255),
+  id_usuario INT,
+  data_publicacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
+  );
 
 
